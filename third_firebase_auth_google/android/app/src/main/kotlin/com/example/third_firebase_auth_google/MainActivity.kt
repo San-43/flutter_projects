82089ff6531362119c48d27e8e717f466105389a
Sandro Martínez <virgosmp.mx@gmail.com>
2025-03-06 20:05:43 -0600

    oops

--- third_firebase_auth_google/android/app/src/main/kotlin/com/example/third_firebase_auth_google/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.third_firebase_auth_google
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- veterinary/android/app/src/main/kotlin/com/example/veterinary/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.veterinary
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
