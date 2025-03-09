9d1f16aa67bb551b60160e05ac312b9636e6ffbe
Sandro Martínez <virgosmp.mx@gmail.com>
2025-03-09 12:30:14 -0600

    a

---
+++ expense_tracker/lib/expenses_list.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+import 'models/expense.dart';
+
+class ExpensesList extends StatelessWidget {
+
+  
+
+  final List<Expense> expenses;
+
+
+  @override
+  Widget build(BuildContext context) {
+
+    return const Text('Expenses List');
+  }
+}
