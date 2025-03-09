1e82a5a8db0cd39b17e900957177dd571f2e968e
Sandro Martínez <virgosmp.mx@gmail.com>
2025-03-08 23:01:25 -0600

    end

--- quiz_app/lib/questions_summary/questions_summary.dart
+++ quiz_app/lib/questions_summary/questions_summary.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:quiz_app/questions_summary/summary_item.dart';
 
 class QuestionsSummary extends StatelessWidget {
