df47b07972f97a6eab028d922cf5f18b6d014789
Sandro Martínez <virgosmp.mx@gmail.com>
2025-03-10 20:25:58 -0600

    Update new_expense.dart

--- expense_tracker/lib/widgets/new_expense.dart
+++ expense_tracker/lib/widgets/new_expense.dart
@@ -1,5 +1,6 @@
 import 'package:expense_tracker/models/expense.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class NewExpense extends StatefulWidget {
   const NewExpense({super.key});
@@ -11,7 +12,8 @@ class NewExpense extends StatefulWidget {
 class _NewExpenseState extends State<NewExpense> {
   final _titleController = TextEditingController();
   final _amountController = TextEditingController();
-  DateTime? _selectedDate;
+  Category _selectedCategory = Category.leisure;
+  DateTime? _selectedDate = DateTime.now();
 
   void _presentDatePicker() async {
     final firstDate = DateTime.now().subtract(const Duration(days: 365));
@@ -25,6 +27,34 @@ class _NewExpenseState extends State<NewExpense> {
     setState(() {
       _selectedDate = picketDate;
     });
+  }
+
+  void _submitExpenseData() {
+    final enteredAmount = double.tryParse(_amountController.text);
+    final amountIsInvalid = enteredAmount == null || enteredAmount <= 0;
+    if (_titleController.text.trim().isEmpty ||
+        amountIsInvalid ||
+        _selectedDate == null) {
+      showDialog(
+        context: context,
+        builder:
+            (ctx) => AlertDialog(
+              title: const Text('Invalid input'),
+              content: const Text(
+                'Please make sure a valid title, amount, date and category was entered.',
+              ),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(ctx);
+                  },
+                  child: const Text('Okay'),
+                ),
+              ],
+            ),
+      );
+      return;
+    }
   }
 
   @override
@@ -46,6 +76,7 @@ class _NewExpenseState extends State<NewExpense> {
             decoration: const InputDecoration(
               label: Text('Title'),
               hintText: 'Enter title',
+              border: UnderlineInputBorder(),
             ),
           ),
           Row(
@@ -53,10 +84,14 @@ class _NewExpenseState extends State<NewExpense> {
               Expanded(
                 child: TextField(
                   controller: _amountController,
-                  keyboardType: TextInputType.number,
+                  keyboardType: TextInputType.numberWithOptions(decimal: true),
+                  inputFormatters: [
+                    FilteringTextInputFormatter.allow(RegExp(r'^\d*\.?\d*$')),
+                  ],
                   decoration: const InputDecoration(
                     prefixText: '\$ ',
                     label: Text('Amount'),
+                    border: OutlineInputBorder(),
                   ),
                 ),
               ),
@@ -76,9 +111,11 @@ class _NewExpenseState extends State<NewExpense> {
               ),
             ],
           ),
+          const SizedBox(height: 16),
           Row(
             children: [
               DropdownButton(
+                value: _selectedCategory,
                 items:
                     Category.values
                         .map(
@@ -89,9 +126,15 @@ class _NewExpenseState extends State<NewExpense> {
                         )
                         .toList(),
                 onChanged: (value) {
-
+                  if (value == null) {
+                    return;
+                  }
+                  setState(() {
+                    _selectedCategory = value;
+                  });
                 },
               ),
+              const Spacer(),
               TextButton(
                 onPressed: () {
                   Navigator.pop(context);
@@ -99,10 +142,7 @@ class _NewExpenseState extends State<NewExpense> {
                 child: const Text('Cancel'),
               ),
               ElevatedButton(
-                onPressed: () {
-                  print(_titleController.text);
-                  print(_titleController.text);
-                },
+                onPressed: _submitExpenseData,
                 child: const Text('Save Expense'),
               ),
             ],
