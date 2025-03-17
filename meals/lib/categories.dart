df39da9bc2870dda416b714f0ec319ada04b0ce6
Sandro Martínez <virgosmp.mx@gmail.com>
2025-03-16 22:09:39 -0600

    working

---
+++ meals/lib/categories.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pick your category'),
+        centerTitle: true,
+      ),
+      body: GridView(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 1.5,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        children: [
+          Text('1', style: TextStyle(color: Colors.white)),
+          Text('1', style: TextStyle(color: Colors.white)),
+          Text('1', style: TextStyle(color: Colors.white)),
+          Text('1', style: TextStyle(color: Colors.white)),
+          Text('1', style: TextStyle(color: Colors.white)),
+          Text('1', style: TextStyle(color: Colors.white)),
+          Text('1', style: TextStyle(color: Colors.white)),
+          Text('1', style: TextStyle(color: Colors.white)),
+        ],
+      ),
+    );
+  }
+}
