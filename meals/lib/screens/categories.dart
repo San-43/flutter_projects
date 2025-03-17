6d5e302341a36da4dfc2e222453539dfbb98dc19
Sandro Martínez <virgosmp.mx@gmail.com>
2025-03-17 00:12:33 -0600

    meal_item widget

--- meals/lib/screens/categories.dart
+++ meals/lib/screens/categories.dart
@@ -1,17 +1,32 @@
 import 'package:flutter/material.dart';
 
 import '../data/dummy_data.dart';
+import '../models/category.dart';
 import '../widgets/category_grid_item.dart';
+import 'meals.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
+  void _selectCategory(BuildContext context, Category category) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder:
+            (ctx) => MealsScreen(
+              title: category.title,
+              meals:
+                  dummyMeals
+                      .where((meal) => meal.categories.contains(category.id))
+                      .toList(),
+            ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Pick your category'),
-      ),
+      appBar: AppBar(title: const Text('Pick your category')),
       body: GridView(
         padding: const EdgeInsets.all(24),
         shrinkWrap: true,
@@ -23,7 +38,12 @@ class CategoriesScreen extends StatelessWidget {
         ),
         children: [
           for (final category in availableCategories)
-            CategoryGridItem(category: category),
+            CategoryGridItem(
+              category: category,
+              onSelectCategory: () {
+                _selectCategory(context, category);
+              },
+            ),
         ],
       ),
     );
